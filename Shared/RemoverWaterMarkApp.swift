45c5c5f7ac47b6459dc3b9369d76704eb0cecc3d
zhenheng <zhenheng@tencent.com>
2022-04-24 11:04:58 +0800

    Initial Commit

---
+++ Shared/RemoverWaterMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoverWaterMarkApp.swift
+//  Shared
+//
+//  Created by sofent on 2022/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoverWaterMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
