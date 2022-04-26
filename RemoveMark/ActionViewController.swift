1073ef710bf6c03fa1f977ceea64bca458dcf999
zhenheng <zhenheng@tencent.com>
2022-04-26 19:56:25 +0800

    add extension

---
+++ RemoveMark/ActionViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ActionViewController.swift
+//  RemoveMark
+//
+//  Created by sofent on 2022/4/26.
+//
+
+import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        var imageFound = false
+        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
+            for provider in item.attachments! {
+                if provider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
+
+                    provider.loadItem(forTypeIdentifier: UTType.image.identifier, options: nil, completionHandler: { (imageURL, error) in
+                        OperationQueue.main.addOperation {
+                            
+                            if let imageURL = imageURL as? URL {
+                                let defaults = UserDefaults(suiteName: "3HG5533YQ5.group.3HG5533YQ5.sofentapp")
+                                defaults!.set("hhhh", forKey: "myfile")
+                                print(imageURL.absoluteString)
+                                self.openContainerApp(imageURL.absoluteString)
+                            }
+                            
+                        }
+                    })
+                    
+                    imageFound = true
+                    break
+                }
+            }
+            
+            if (imageFound) {
+                // We only handle one image, so stop looking for more.
+                break
+            }
+        }
+        self.done()
+    }
+    // For skip compile error.
+    @objc func openURL(_ url: URL) {
+        return
+    }
+
+    
+    func openContainerApp(_ url:String) {
+        var responder: UIResponder? = self as UIResponder
+        let selector = #selector(openURL(_:))
+        while responder != nil {
+            if responder!.responds(to: selector) && responder != self {
+                responder!.perform(selector, with: URL(string: "sremovemk://"+url.urlEncoded!)!)
+                return
+            }
+            responder = responder?.next
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Get the item[s] we're handling from the extension context.
+        
+        // For example, look for an image and place it into an image view.
+        // Replace this with something appropriate for the type[s] your extension supports.
+        
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
+
+extension String {
+    var urlEncoded: String? {
+        let allowedCharacterSet = CharacterSet.alphanumerics.union(CharacterSet(charactersIn: "~-_."))
+        return self.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)
+    }
+}
--- Shared/RemoverWaterMarkApp.swift
+++ Shared/RemoverWaterMarkApp.swift
@@ -9,12 +9,42 @@ import SwiftUI
 
 @main
 struct RemoverWaterMarkApp: App {
+    var bodyView = ContentView()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            bodyView
                 .onOpenURL { url in
-                    print(url)
+                    if url.scheme=="sremovemk" {
+                        let imageUrl=url.host?.removingPercentEncoding
+                        fetchImage(URL(string:imageUrl!))
+                        
+                    }
+                    
                 }
+        }
+    }
+    
+    private func fetchImage(_ photoURL: URL?) {
+
+        guard let imageURL = photoURL else { return  }
+       
+        DispatchQueue.global(qos: .userInitiated).async {
+            let defaults = UserDefaults(suiteName: "3HG5533YQ5.group.3HG5533YQ5.sofentapp")
+            print(defaults!.synchronize())
+            print(imageURL.absoluteString)
+            print(defaults?.dictionaryRepresentation())
+            let d = defaults!.string(forKey: "myfile")
+            print(d as Any)
+            do{
+                let imageData: Data = try Data(contentsOf: imageURL)
+
+                DispatchQueue.main.async {
+                    let image = UIImage(data: imageData)
+                    self.bodyView.image = image
+                }
+            }catch{
+                    print("Unable to load data: \(error)")
+            }
         }
     }
 }
